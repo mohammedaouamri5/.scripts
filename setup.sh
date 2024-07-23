abbf3a08c3689dfb4f73fd6b76e8c61fd548a2c2
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-07-23 01:31:55 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ echo $HOME
 # sudo swapon /dev/nvme0n1p3; 
 swww init;
 swww-daemon --format xrgb;
+
+
+export PATH=$PATH:$HOME/bin/Vscode/bin
+export SCRIPTS="$HOME/.scripts" 
+export PATH=$PATH:$SCRIPTS
