53800ce130ff8f8cf413827611905465aaee1d92
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2025-03-15 22:57:23 +0100

    add scripts from arch-hyprland

---
+++ WallpaperSelect.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */ 
+# This script for selecting wallpapers (SUPER W)
+
+# WALLPAPERS PATH
+terminal=konsole
+wallDIR="$HOME/Pictures/wallpapers"
+SCRIPTSDIR="$HOME/.scripts"
+wallpaper_current="$HOME/.config/hypr/wallpaper_effects/.wallpaper_current"
+
+# Directory for swaync
+iDIR="$HOME/.config/swaync/images"
+iDIRi="$HOME/.config/swaync/icons"
+
+# Check if package bc exists
+if ! command -v bc &>/dev/null; then
+notify-send -i "$iDIR/ja.png" "bc missing" "Install package bc first"
+exit 1
+fi
+
+# variables
+rofi_theme="$HOME/.config/rofi/config-wallpaper.rasi"
+focused_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+
+# Monitor details
+scale_factor=$(hyprctl monitors -j | jq -r --arg mon "$focused_monitor" '.[] | select(.name == $mon) | .scale')
+monitor_height=$(hyprctl monitors -j | jq -r --arg mon "$focused_monitor" '.[] | select(.name == $mon) | .height')
+
+icon_size=$(echo "scale=1; ($monitor_height * 3) / ($scale_factor * 150)" | bc)
+
+# Apply limit
+adjusted_icon_size=$(echo "$icon_size" | awk '{if ($1 < 15) $1 = 20; if ($1 > 25) $1 = 25; print $1}')
+
+# Setting the rofi override with the adjusted icon size
+rofi_override="element-icon{size:${adjusted_icon_size}%;}"
+
+# swww transition config
+FPS=60
+TYPE="any"
+DURATION=2
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
+
+# Check if swaybg is running
+if pidof swaybg > /dev/null; then
+  pkill swaybg
+fi
+
+# Retrieve image files using null delimiter to handle spaces in filenames
+mapfile -d '' PICS < <(find -L "${wallDIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.pnm" -o -iname "*.tga" -o -iname "*.tiff" -o -iname "*.webp" -o -iname "*.bmp" -o -iname "*.farbfeld" -o -iname "*.png" -o -iname "*.gif" \) -print0)
+
+RANDOM_PIC="${PICS[$((RANDOM % ${#PICS[@]}))]}"
+RANDOM_PIC_NAME=". random"
+
+# Rofi command
+rofi_command="rofi -i -show -dmenu -config $rofi_theme -theme-str $rofi_override"
+
+# Sorting Wallpapers
+menu() {
+  # Sort the PICS array
+  IFS=$'\n' sorted_options=($(sort <<<"${PICS[*]}"))
+  
+  # Place ". random" at the beginning with the random picture as an icon
+  printf "%s\x00icon\x1f%s\n" "$RANDOM_PIC_NAME" "$RANDOM_PIC"
+  
+  for pic_path in "${sorted_options[@]}"; do
+    pic_name=$(basename "$pic_path")
+    
+    # Displaying .gif to indicate animated images
+    if [[ ! "$pic_name" =~ \.gif$ ]]; then
+      printf "%s\x00icon\x1f%s\n" "$(echo "$pic_name" | cut -d. -f1)" "$pic_path"
+    else
+      printf "%s\n" "$pic_name"
+    fi
+  done
+}
+
+# initiate swww if not running
+swww query || swww-daemon --format xrgb
+
+# Choice of wallpapers
+main() {
+  choice=$(menu | $rofi_command)
+  
+  choice=$(echo "$choice" | xargs)
+  RANDOM_PIC_NAME=$(echo "$RANDOM_PIC_NAME" | xargs)
+
+  # No choice case
+  if [[ -z "$choice" ]]; then
+    echo "No choice selected. Exiting."
+    exit 0
+  fi
+
+  # Random choice case
+  if [[ "$choice" == "$RANDOM_PIC_NAME" ]]; then
+	swww img -o "$focused_monitor" "$RANDOM_PIC" $SWWW_PARAMS;
+    sleep 2
+    "$SCRIPTSDIR/WallustSwww.sh"
+    exit 0
+  fi
+
+  pic_index=-1
+  for i in "${!PICS[@]}"; do
+    filename=$(basename "${PICS[$i]}")
+    if [[ "$filename" == "$choice"* ]]; then
+      pic_index=$i
+      break
+    fi
+  done
+
+  if [[ $pic_index -ne -1 ]]; then
+    swww img -o "$focused_monitor" "${PICS[$pic_index]}" $SWWW_PARAMS
+  else
+    echo "Image not found."
+    exit 1
+  fi
+}
+
+# Check if rofi is already running
+if pidof rofi > /dev/null; then
+  pkill rofi
+fi
+
+main
+
+wait $!
+"$SCRIPTSDIR/WallustSwww.sh" &&
+
+
+sleep 1
+# Check if user selected a wallpaper
+if [[ -n "$choice" ]]; then
+  sddm_sequoia="/usr/share/sddm/themes/sequoia_2"
+  if [ -d "$sddm_sequoia" ]; then
+  
+	# Check if yad is running to avoid multiple yad notification
+	if pidof yad > /dev/null; then
+	  killall yad
+	fi
+	 cp -r $wallpaper_current '$sddm_sequoia/backgrounds/default'
+  fi
+fi
